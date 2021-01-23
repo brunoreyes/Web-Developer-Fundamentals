053110c1deb6c7f26332263338e4135c3bb510b8
brunoreyes <bruno619reyes@gmail.com>
2021-01-23 14:11:20 -0600

    Completed a basic pokemon for loop challenge, using DOM manipulation methods to create a label and image for each pokemon, place that within an induvidual pokemon container, and loop that and append it to an even bigger container to display all 151 series 1 pokemon (displaying an img and corresponding #)

--- DOM_Intro_CODE/Pokemon/app.js
+++ DOM_Intro_CODE/Pokemon/app.js
@@ -1,19 +1,19 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
 const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
-
+const baseURL =
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 for (let i = 1; i <= 151; i++) {
-    const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
+  const pokemon = document.createElement('div'); // creating a conatiner for each pokemon
+  pokemon.classList.add('pokemon'); // adding the class association pokemon to the container div
+  const label = document.createElement('span'); // creating a span to display each pokemon's #
+  label.innerText = `#${i}`; // looping the pokemon number insive the span element
+  const newImg = document.createElement('img'); // creating an image element for each poke
+  // looping the base url & adding the #.png to grab the unique pokemon src & appending it to the img element
+  newImg.src = `${baseURL}${i}.png`;
 
-
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon)
+  pokemon.appendChild(newImg); // adding the img to the indivudual pokemon div
+  pokemon.appendChild(label); // adding the label to the indivudual pokemon div
+  container.appendChild(pokemon); // adding the individual pokemon container and all it's children to the big #container
 }
