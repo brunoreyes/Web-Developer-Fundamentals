92879e29962d17320da9acdab19d8fc80856c8a9
brunoreyes <bruno619reyes@gmail.com>
2021-01-26 15:01:11 -0600

    Reviewing AJAC and API terminology

---
+++ scripts/AJAXandAPIs.js
@@ -0,0 +1,20 @@
+// Within browser inspector >> network >> I can view search queries I'm running in real-time
+
+// What I send a requested query I get a response back in the form of data formatted in JSON
+
+// The concept behind AJAX is creating applications using JavaScript, we can load, save, send,
+// or save to a database without having to refresh or open up another page
+
+// multiple request can be sent and used within the same page
+
+// Web API's are like web-based interfaces that expose certain endpoints that
+// will respond with information (data) for other code or software to utilize
+
+// Essentialy Web API's are like portals into different applications, database or data sets or niche data.
+// Web API's are interfaces that occur over HTTPS to particular URL's called endpoints
+
+// Sending an API request by opening browser and sending request link:
+
+// Request: https://api.cryptonator.com/api/ticker/btc-usd
+// Response (data within JSON format): {"ticker":{"base":"BTC","target":"USD","price":"32049.96352855",
+//"volume": "57161.45837257", "change": "-328.78579545"}, "timestamp": 1611694742, "success": true, "error": ""}
