0c8a3a296dfe61bc24ce094c8f0bf68888ca84fa
brunoreyes <bruno619reyes@gmail.com>
2021-02-23 21:46:58 -0600

    Learning differences between frameworks and libraries and how express is a quick minamilist that is a framework

---
+++ scripts/expressIntroduction.js
@@ -0,0 +1,17 @@
+// Express helps us to:
+// - start up a server to listen for requests
+// - parse incoming requests
+// - match those requests to particular routes
+// - craft our http response and associated content
+
+// Libraries vs. Frameworks
+
+// Library, when you use one, you are in charge!
+// You control the flow of the application code,
+// and you decide when to use the library.
+// for ex: axios, color generators, jQuery
+
+// With Frameworks, the control is inverted.
+// The framework is in charge, and you are merely a participant!
+// The framework tells you where to plug in your code.
+// for ex: express, ruby on rails,
