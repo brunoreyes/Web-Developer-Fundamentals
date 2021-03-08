81ca18757bbc35c02e76636a88a1180aae28c71c
brunoreyes <bruno619reyes@gmail.com>
2021-03-07 22:49:44 -0600

    Worked on sending multiple types of request, reading in http responses via postman, and getting use to dealing with response objects

--- veryFirstApp/index.js
+++ veryFirstApp/index.js
@@ -4,14 +4,16 @@ const app = express();
 
 // the terminal command "node index.js" will run this document
 
-app.use(() => { // anytime there's an incomming request, this callback will run
-    console.log('We got a new request')  
-})
+app.use((req, res) => {
+  // anytime there's an incomming request, this callback will run
+  console.log('We got a new request');
+  //   console.dir(req);
+  //   res.send('Hello, we got your request! This is a response'); // res.send() sends and generates an http response
+  res.send('<h1>This is my webpage!</h1>');
+}); // express turns http text into a parsed object that is passed into an object passed in by app.use
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+app.listen(8080, () => {
+  console.log('Listening on port 8080!');
 });
 
-
-
-
+// sending a request by searching "http://localhost:3000/" in the browser
