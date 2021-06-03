fe7d9f8694946f70f940504148e4d4e196261f2c
brunoreyes <bruno619reyes@gmail.com>
2021-06-02 22:38:47 -0500

    Built POST routes to create new comments as well as GET route to display list of comments

--- RESTDemo/index.js
+++ RESTDemo/index.js
@@ -5,13 +5,54 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true })); // parses any data coming in from a form
+app.use(express.json());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+  {
+    username: 'Todd',
+    comment: 'lol that is so funny!',
+  },
+  {
+    username: 'Skyler',
+    comment: 'I like to go birdwatching with my dog',
+  },
+  {
+    username: 'Sk8erBoi',
+    comment: 'Plz delete your account, Todd',
+  },
+  {
+    username: 'onlysayswoof',
+    comment: 'woof woof woof',
+  },
+];
+
+app.get('/comments', (req, res) => {
+  res.render('comments/index', { comments });
+});
+
+app.get('/comments/new', (req, res) => {
+  res.render('comments/new');
+});
+
+app.post('/comments', (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.send('It Worked!!');
+});
 
 app.get('/tacos', (req, res) => {
   res.send('GET /tacos response');
 });
 
 app.post('/tacos', (req, res) => {
-  res.send('POST /tacos response');
+  const { meat, qty } = req.body;
+  res.send(`OK, here are your ${qty} ${meat} tacos`);
 });
 
 // Setting our app to run on port 3000
